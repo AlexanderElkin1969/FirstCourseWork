e5c3bc82b35756360694a26bfbc7b24e0023d7a1
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-02-06 19:47:10 +0300

    print_list min max

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import BaseDifficulty.Employee;
 public class Main {
     public static void main(String[] args) {
         String[] surnames = {"Александров", "Алексеев", "Борисов", "Иванов", "Владимиров", "Дмитриев", "Егоров", "Константинов", "Михайлов", "Петров"};
-        String[] names = {"Александр", "Алексей", "Борис", "Виктор", "Владимир", "Дмитрий", "Егор", "Константин", "Михаил", "Петр"};
-        String[] midnames = {"Александрович", "Алексеевич", "Борисович", "Викторович", "Владимирович", "Дмитриевич", "Егорович", "Константинович",
+        String[] names = {"Александр", "Алексей", "Борис", "Иван", "Владимир", "Дмитрий", "Егор", "Константин", "Михаил", "Петр"};
+        String[] midnames = {"Александрович", "Алексеевич", "Борисович", "Иванович", "Владимирович", "Дмитриевич", "Егорович", "Константинович",
                 "Михайлович", "Петрович"};
-        int l, n, m, d, s;
+        int l, n, m, d, s, id;
         Employee[] employee = new Employee[10];
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++) {               /* заполняем массив случайными значениями */
             l = (int) (Math.random() * 10);
             n = (int) (Math.random() * 10);
             m = (int) (Math.random() * 10);
@@ -16,8 +16,17 @@ public class Main {
             s = 100_000 + (int) (Math.random() * 100_001);
             employee[i] = new Employee(surnames[l], names[n], midnames[m], d, s);
         }
-        printLFullInfoEployee(employee);
         printListEmployee(employee);
+        printLFullInfoEployee(employee);
+        System.out.println("Сотрудник с минимальной зарплатой:");
+        id = findMinSalary(employee);
+        System.out.println(employee[id - 1]);
+        s = employee[id-1].getSalary();
+        employee[id -1].setSalary(s + 20_000);       /*   повышаем зарплату на 20 000 рублей  */
+        System.out.println("После повышения зарплаты на 20 000 рублей :");
+        System.out.println(employee[id - 1]);
+        System.out.println("Сотрудник с максимальной зарплатой:");
+        System.out.println(employee[findMaxSalary(employee) -1]);
     }
     public static void printLFullInfoEployee(Employee[] employee) {
         System.out.println("Полная информация о сотрудниках:");
@@ -25,6 +34,30 @@ public class Main {
         for (int i = 0; i < s; i++) {
             System.out.println(employee[i]);
         }
+    }
+    public static int findMinSalary(Employee[] employee) {
+        int s = employee[0].getSize();
+        int min = 200_000;
+        int id = 0;
+        for (int i = 0; i < s; i++) {
+            if (employee[i].getSalary() < min) {
+                min = employee[i].getSalary();
+                id =i+1;
+            }
+        }
+        return id;
+    }
+    public static int findMaxSalary(Employee[] employee) {
+        int s = employee[0].getSize();
+        int max = 0;
+        int id = 0;
+        for (int i = 0; i < s; i++) {
+            if (employee[i].getSalary() > max) {
+                max = employee[i].getSalary();
+                id =i+1;
+            }
+        }
+        return id;
     }
     public static void printListEmployee(Employee[] employee) {
         System.out.println("Список сотрудников:");
